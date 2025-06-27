57084aa1d0268da1917a1afd1d2a18b27708fabc
architajain2003 <59245825+architajain2003@users.noreply.github.com>
2025-06-27 16:42:54 +0530

    vapi addition

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Agent from '@/components/Agents'
+import { getCurrentUser } from '@/lib/actions/auth.actions'
 
-const page = () => {
+// interface AgentProps {
+//   userName: string;
+//   userId: string;
+//   type: "generate" | "somethingElse"; // use actual valid values
+// }
+
+const Page = async() => {
+
+  const user = await getCurrentUser();
+
   return (
     <>
     <h3>Interview Generation</h3>
-    <Agent userName="You" userId="user1" type="generate"></Agent>
+    <Agent userName={user?.name} userId={user?.id} type="generate"></Agent>
     </>
   )
 }
 
-export default page
+export default Page
